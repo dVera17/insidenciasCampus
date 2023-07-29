825a6c1fc85cff5a33d64d020d8757ea875c6758
dVera17 <dsvera174@gmail.com>
2023-07-29 14:49:16 -0500

    feat: :sparkles: configuracion inicial del endpoint area (tambien tipo_area)
    configuracion inicial del endpoint area (tambien tipo_area)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { config } from 'dotenv';
+import routerArea from './routes/area.routes.js';
 config();
 const app = express();
 app.set('port', process.env.PORT_SERVER);
+app.use(express.json());
+
+app.use('/api/area', routerArea);
 
 export default app;
