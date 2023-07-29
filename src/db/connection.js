2495e76a31a10dff3d57ff97fcfa857f911c8211
dVera17 <dsvera174@gmail.com>
2023-07-29 14:37:53 -0500

    feat: :sparkles: configuracion conexion a la base de datos
    configuracion conexion a la base de datos

---
+++ src/db/connection.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql2';
+import config from '../config.js';
+
+const conn = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+})
+
+export default conn;
